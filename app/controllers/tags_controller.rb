9bb08e38350d79eb419a496d7293cfc9429bf475
Sachin Srivastava <sachinrising@gmail.com>
2016-02-11 15:51:49 +0530

    Tagging almost done only destroy is left

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -2,4 +2,17 @@ class TagsController < ApplicationController
 	def show
 		@tag = Tag.find_by(id: params[:id])
 	end
+
+	def index
+		@tags = Tag.all
+	end
+
+	def destroy
+		@taggers = Tagging.find_by(tag_id: params[:id])
+		@taggers.destroy
+		@tag = Tag.find_by(id: params[:id])
+		@tag.destroy
+
+		redirect_to tags_path
+	end
 end
